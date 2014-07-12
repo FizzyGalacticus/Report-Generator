bc7e94a2b657c169e2cd4a64b78f2a661282f922
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-07-11 17:34:38 -0800

    Figured out how to take user input with a QInputDialog object.

--- Qt/mainwindow.cpp
+++ Qt/mainwindow.cpp
@@ -1,8 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 #include <QPushButton>
 #include <QRect>
 #include <QDesktopWidget>
+#include <QInputDialog>
+#include <QString>
+#include <string>
+using std::string;
+
+const QString getUserInput(QWidget *parent, const QString windowName, const QString prompt)
+{
+    return QInputDialog::getText(parent, windowName,prompt, QLineEdit::Normal,"", 0);
+}
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,11 +21,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->setWindowTitle("Report Generator");
 
+    const QString initials = getUserInput(this, "Initials", "Initials:");
+
     QDesktopWidget desktop;
     QWidget *wdg = new QWidget;
     wdg->setWindowTitle("Pop-up");
-    //wdg->geometry().setHeight(200);
-    //wdg->geometry().setWidth(200);
     wdg->setFixedSize(50,40);
 
     QPushButton *myButton = new QPushButton(wdg);
