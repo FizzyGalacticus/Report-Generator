08f8de32a06a75da53ea489e8ac83f6ff269692e
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-07-24 07:21:34 -0800

    Swapped order of checkboxes.

--- Qt/checkboxes.cpp
+++ Qt/checkboxes.cpp
@@ -5,19 +5,40 @@
 
 void MainWindow::_setupCheckboxes()
 {
+    _setupAddInitialsCheckbox();
+    _setupDateAndTimeCheckboxes();
     _setupCheckoutCheckbox();
     _setupHDDCheckboxes();
     _setupSFCCheckboxes();
-    _setupAddInitialsCheckbox();
-    _setupDateAndTimeCheckboxes();
+}
+
+void MainWindow::_setupAddInitialsCheckbox()
+{
+    _addInitials = new QCheckBox(this);
+    _addInitials->setText("Add Initials");
+    const int addinitialsY = _initialsListView->geometry().y()+_initialsListView->height()+3;
+    _addInitials->setGeometry(0,addinitialsY,width()/3,_initials->height()+3);
+    connect(_addInitials, SIGNAL(stateChanged(int)),this,SLOT(_addInitialsStateHasChanged(int)));
+}
+
+void MainWindow::_setupDateAndTimeCheckboxes()
+{
+    _addDate = new QCheckBox(this);
+    _addDate->setText("Add Date");
+    _addDate->setGeometry(_addInitials->geometry().x(),_addInitials->geometry().y()+_addInitials->height()+3,_addInitials->width(),_addInitials->height());
+    connect(_addDate, SIGNAL(stateChanged(int)),this,SLOT(_addDateStateHasChanged(int)));
+
+    _addTime = new QCheckBox(this);
+    _addTime->setText("Add Time");
+    _addTime->setGeometry(_addInitials->geometry().x(),_addDate->geometry().y()+_addDate->height()+3,_addInitials->width(),_addInitials->height());
+    connect(_addTime, SIGNAL(stateChanged(int)),this,SLOT(_addTimeStateHasChanged(int)));
 }
 
 void MainWindow::_setupCheckoutCheckbox()
 {
     _checkout = new QCheckBox(this);
     _checkout->setText("Just Checked Out");
-    const int checkoutY = _initialsListView->geometry().y()+_initialsListView->height()+3;
-    _checkout->setGeometry(0,checkoutY,width()/3,_initials->height()+3);
+    _checkout->setGeometry(_addInitials->width(),_addInitials->geometry().y(),_addInitials->width(),_addInitials->height());
     connect(_checkout, SIGNAL(stateChanged(int)),this,SLOT(_checkoutStateHasChanged(int)));
 }
 
@@ -27,14 +48,14 @@ void MainWindow::_setupHDDCheckboxes()
     _hddscan = new QCheckBox(this);
     _hddscan->setText("HDD Scan");
     const int hddscanY = _checkout->geometry().y() + _checkout->height() + 3;
-    _hddscan->setGeometry(0,hddscanY,_checkout->width(),_checkout->height());
+    _hddscan->setGeometry(_checkout->geometry().x(),hddscanY,_checkout->width(),_checkout->height());
     connect(_hddscan,SIGNAL(stateChanged(int)),this,SLOT(_hddscanStateHasChanged(int)));
 
     //     HDD Scan Pass Checkbox
     _hddpass = new QCheckBox(this);
     _hddpass->setText("Passed");
     const int hddpassY = _hddscan->geometry().y() + _hddscan->height() + 3;
-    _hddpass->setGeometry(15,hddpassY,_hddscan->width()-15,_hddscan->height());
+    _hddpass->setGeometry(_hddscan->geometry().x()+15,hddpassY,_hddscan->width()-15,_hddscan->height());
     _hddpass->setCheckable(false);
     _hddpass->setVisible(false);
     connect(_hddpass,SIGNAL(stateChanged(int)),this,SLOT(_hddpassStateHasChanged(int)));
@@ -43,7 +64,7 @@ void MainWindow::_setupHDDCheckboxes()
     _hddfail = new QCheckBox(this);
     _hddfail->setText("Failed");
     const int hddfailY = _hddpass->geometry().y() + _hddscan->height() + 3;
-    _hddfail->setGeometry(15,hddfailY,_hddpass->width(),_hddscan->height());
+    _hddfail->setGeometry(_hddscan->geometry().x()+15,hddfailY,_hddpass->width(),_hddscan->height());
     _hddfail->setCheckable(false);
     _hddfail->setVisible(false);
     connect(_hddfail,SIGNAL(stateChanged(int)),this,SLOT(_hddfailStateHasChanged(int)));
@@ -55,14 +76,14 @@ void MainWindow::_setupSFCCheckboxes()
     _sfcscan = new QCheckBox(this);
     _sfcscan->setText("SFC Scan");
     const int sfcscanY = _hddfail->geometry().y() + _hddfail->height() + 3;
-    _sfcscan->setGeometry(0,sfcscanY,_hddfail->width(),_checkout->height());
+    _sfcscan->setGeometry(_hddscan->geometry().x(),sfcscanY,_hddfail->width(),_checkout->height());
     connect(_sfcscan,SIGNAL(stateChanged(int)),this,SLOT(_sfcscanStateHasChanged(int)));
 
     //     SFC Scan Pass Checkbox
     _sfcpass = new QCheckBox(this);
     _sfcpass->setText("Passed");
     const int sfcpassY = _sfcscan->geometry().y() + _sfcscan->height() + 3;
-    _sfcpass->setGeometry(15,sfcpassY,_hddpass->width(),_sfcscan->height());
+    _sfcpass->setGeometry(_hddscan->geometry().x()+15,sfcpassY,_hddpass->width(),_sfcscan->height());
     _sfcpass->setCheckable(false);
     _sfcpass->setVisible(false);
     connect(_sfcpass,SIGNAL(stateChanged(int)),this,SLOT(_sfcpassStateHasChanged(int)));
@@ -71,31 +92,10 @@ void MainWindow::_setupSFCCheckboxes()
     _sfcfail = new QCheckBox(this);
     _sfcfail->setText("Failed");
     const int sfcfailY = _sfcpass->geometry().y() + _sfcscan->height() + 3;
-    _sfcfail->setGeometry(15,sfcfailY,_hddpass->width(),_sfcscan->height());
+    _sfcfail->setGeometry(_hddscan->geometry().x()+15,sfcfailY,_hddpass->width(),_sfcscan->height());
     _sfcfail->setCheckable(false);
     _sfcfail->setVisible(false);
     connect(_sfcfail,SIGNAL(stateChanged(int)),this,SLOT(_sfcfailStateHasChanged(int)));
-}
-
-void MainWindow::_setupAddInitialsCheckbox()
-{
-    _addInitials = new QCheckBox(this);
-    _addInitials->setText("Add Initials");
-    _addInitials->setGeometry(_checkout->width()+3,_checkout->geometry().y(),_checkout->width(),_checkout->height());
-    connect(_addInitials, SIGNAL(stateChanged(int)),this,SLOT(_addInitialsStateHasChanged(int)));
-}
-
-void MainWindow::_setupDateAndTimeCheckboxes()
-{
-    _addDate = new QCheckBox(this);
-    _addDate->setText("Add Date");
-    _addDate->setGeometry(_addInitials->geometry().x(),_hddscan->geometry().y(),_checkout->width(),_checkout->height());
-    connect(_addDate, SIGNAL(stateChanged(int)),this,SLOT(_addDateStateHasChanged(int)));
-
-    _addTime = new QCheckBox(this);
-    _addTime->setText("Add Time");
-    _addTime->setGeometry(_addInitials->geometry().x(),_hddpass->geometry().y(),_checkout->width(),_checkout->height());
-    connect(_addTime, SIGNAL(stateChanged(int)),this,SLOT(_addTimeStateHasChanged(int)));
 }
 
 /**************     SLOTS   ************************/
