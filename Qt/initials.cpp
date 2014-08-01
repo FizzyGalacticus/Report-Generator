dbc6694f136249a4b5991ab8562c6ceb3e4a7de3
Dustin L. Dodson <dustinleedodson@gmail.com>
2014-08-01 08:28:26 -0800

    Added the tr() to all strings in initials.cpp.

--- Qt/initials.cpp
+++ Qt/initials.cpp
@@ -6,11 +6,11 @@ void MainWindow::_initialsListItemHasBeenClicked(QListWidgetItem * item)
     if(item != _initialsListView->item(_initialsListView->count()-1))
     {
             _currentUser = item->text();
-            qDebug() << _currentUser.toStdString().c_str() << "Clicked!" << '\n';
+            qDebug() << _currentUser.toStdString().c_str() << tr("Clicked!") << '\n';
     }
     else
     {
-        string userInitials = getUserInputString(this,"Initials","Initials:").toStdString();
+        string userInitials = getUserInputString(this,tr("Initials"),tr("Initials:")).toStdString();
         if(userInitials.size())
         {
             appendLineToFile(tr("users"),tr(userInitials.c_str()));
@@ -24,10 +24,10 @@ void MainWindow::_initialsListItemHasBeenClicked(QListWidgetItem * item)
                     _initialsListView->setItemSelected(_initialsListView->item(i), true);
             _currentUser = userInitials.c_str();
 
-            _initialsListView->addItem(QString("Add New..."));
+            _initialsListView->addItem(QString(tr("Add New...")));
             _initialsListView->item(_initialsListView->count()-1)->setTextColor("Black");
 
-            qDebug() << _currentUser.toStdString().c_str() << "Clicked!" << '\n';
+            qDebug() << _currentUser.toStdString().c_str() << tr("Clicked!") << '\n';
         }
     }
 
@@ -50,11 +50,11 @@ void MainWindow::_setupInitials()
                     SLOT(_initialsListItemHasBeenClicked(QListWidgetItem*))
                 );
 
-    vector<string> myList = getStringListFromFile("users");
+    vector<string> myList = getStringListFromFile(tr("users"));
 
     if( myList.empty())
     {
-        string userInitials = getUserInputString(this,"Initials","Initials:").toStdString();
+        string userInitials = getUserInputString(this,tr("Initials"),tr("Initials:")).toStdString();
         myList.push_back(userInitials);
         appendLineToFile(tr("users"),tr(userInitials.c_str()));
     }
@@ -64,7 +64,7 @@ void MainWindow::_setupInitials()
         _initialsListView->addItem(QString(myList[i].c_str()));
         _initialsListView->item(i)->setTextColor("Black");
     }
-    _initialsListView->addItem("Add new...");
+    _initialsListView->addItem(tr("Add new..."));
     _initialsListView->setGeometry(0,_initials->geometry().y()+_initials->geometry().height()+3,146,100);
     _initialsListView->show();
 }
